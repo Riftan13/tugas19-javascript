5b35dce9ffdf548040842a26d33ffe72b427eb73
Riftan13 <55617983+Riftan13@users.noreply.github.com>
2020-09-21 11:42:42 +0700

    Add files via upload

---
+++ tugas19.js
@@ -0,0 +1,19 @@
+var ulang = 'Semangat...'
+var hasil2 = ulang.repeat(5)
+console.log(hasil2)
+
+var ulang1 = '*'
+var hasil3 = ulang1.repeat(5)
+var hasil4 = ulang1.repeat(15)
+var hasil5 = ulang1.repeat(25)
+var hasil6 = ulang1.repeat(40)
+var hasil7 = ulang1.repeat(25)
+var hasil8 = ulang1.repeat(15)
+var hasil9 = ulang1.repeat(5)
+console.log(hasil3)
+console.log(hasil4)
+console.log(hasil5)
+console.log(hasil6)
+console.log(hasil7)
+console.log(hasil8)
+console.log(hasil9)
